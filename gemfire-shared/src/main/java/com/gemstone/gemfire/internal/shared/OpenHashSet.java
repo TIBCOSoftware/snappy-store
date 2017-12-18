32fef0b7138f0bfca068a07d35e7b7fb51cf414f
Sumedh Wale <swale@snappydata.io>
2017-12-19 01:54:38 +0530

    fix OpenHashSet for the case when requested capacity is zero

--- gemfire-shared/src/main/java/com/gemstone/gemfire/internal/shared/OpenHashSet.java
+++ gemfire-shared/src/main/java/com/gemstone/gemfire/internal/shared/OpenHashSet.java
@@ -60,7 +60,7 @@ public class OpenHashSet<E> extends AbstractSet<E>
 
   @SuppressWarnings("unused")
   public OpenHashSet() {
-    this(32, 0.7f);
+    this(16, 0.7f);
   }
 
   public OpenHashSet(int initialCapacity) {
@@ -371,9 +371,11 @@ public class OpenHashSet<E> extends AbstractSet<E>
   protected static int checkCapacity(int capacity) {
     if (capacity > 0 && capacity <= MAX_CAPACITY) {
       return capacity;
+    } else if (capacity == 0) {
+      return 8;
     } else {
-      throw new IllegalStateException("Can't contain more than " +
-          MAX_CAPACITY + " elements");
+      throw new IllegalStateException("Capacity (" + capacity +
+          ") can't be more than " + MAX_CAPACITY + " elements or negative");
     }
   }
 
