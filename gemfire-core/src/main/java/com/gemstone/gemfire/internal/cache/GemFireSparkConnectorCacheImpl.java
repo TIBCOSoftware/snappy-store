f5e093d340f13e51893557ad4b56fb231caa45cb
ahshahid <ashahid@snappydata.io>
2018-01-17 12:59:48 -0800

    making the prefix for connector as snappydata.connector.grid

--- gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/GemFireSparkConnectorCacheImpl.java
+++ gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/GemFireSparkConnectorCacheImpl.java
@@ -30,9 +30,9 @@ public class GemFireSparkConnectorCacheImpl extends GemFireCacheImpl {
 
   // The properties need to be passed from the lead & servers as
   // snappydata.store.connector.gemfire-grid-one and snappydata.store.connector.gemfire.pingInterval etc
-  public static final String connectorPrefix = "connector.";
+  public static final String connectorPrefix = "snappydata.connector.";
   public static final String gfeGridNamePrefix = connectorPrefix + "gemfire-grid";
-  public static final String gfeGridPropsPrefix = connectorPrefix + "gemfire.grid.";
+  public static final String gfeGridPropsPrefix = connectorPrefix + "grid.";
   public static final String propFreeConnTimeout = "freeConnectionTimeout";
   public static final String propLoadConditioningInterval = "loadConditioningInterval";
   public static final String propSocketBufferSize = "socketBufferSize";
