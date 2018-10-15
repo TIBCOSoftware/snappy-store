e3e5e6690f15c79c45fcff72cb3094cbd5a9a75e
ahshahid <ashahid@snappydata.io>
2018-10-15 11:50:55 -0700

    Temporary workaround for SNAP-2627. If the unique constraint violation is due to removed/destroyed AbstractRegionEntry, attempt is made to remove it from index & another try is made to put the new value against the index key.

--- gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/access/operations/SortedMap2IndexInsertOperation.java
+++ gemfirexd/core/src/main/java/com/pivotal/gemfirexd/internal/engine/access/operations/SortedMap2IndexInsertOperation.java
@@ -19,6 +19,7 @@ package com.pivotal.gemfirexd.internal.engine.access.operations;
 
 import java.io.IOException;
 
+import com.gemstone.gemfire.LogWriter;
 import com.gemstone.gemfire.cache.ConflictException;
 import com.gemstone.gemfire.cache.query.IndexMaintenanceException;
 import com.gemstone.gemfire.i18n.LogWriterI18n;
@@ -284,6 +285,29 @@ public final class SortedMap2IndexInsertOperation extends MemIndexOperation {
           }
           break;
         }
+
+        // TODO: identify the cause of Bug SNAP-2627
+        // Till then this is the crude fix
+        if (oldValue instanceof AbstractRegionEntry &&
+            ((AbstractRegionEntry)oldValue).isDestroyedOrRemoved()) {
+          // create a dummy exception & throw to get stack:
+          try {
+            throw GemFireXDUtils.newDuplicateKeyViolation("unique constraint",
+                container.getQualifiedTableName(), "key=" + key.toString()
+                    + ", row=" + value, oldValue, null, null);
+          } catch(Exception e) {
+            final GemFireCacheImpl cache = Misc.getGemFireCache();
+            final LogWriter logger = cache.getLogger();
+
+            logger.error("Unique index constraint violation caused due to " +
+                "removed/destroyed entry. The index is corrupted. Cleaning the index", e);
+
+          }
+
+          skipListMap.remove(key, oldValue);
+          continue;
+        }
+
         throw GemFireXDUtils.newDuplicateKeyViolation("unique constraint",
             container.getQualifiedTableName(), "key=" + key.toString()
                 + ", row=" + value, oldValue, null, null);
